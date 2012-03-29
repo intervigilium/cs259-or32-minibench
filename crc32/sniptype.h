839bdd090ba45bd89fa16460de66c0fd10d8f33a
Ethan Chen <intervigil@gmail.com>
2012-03-28 17:01:07 -0700

    crc_32: format source code

--- crc32/sniptype.h
+++ crc32/sniptype.h
@@ -15,10 +15,10 @@ typedef enum {Error_ = -1, Success_, False_ = 0, True_} Boolean_T;
 /*#if !defined(WIN32) && !defined(_WIN32) && !defined(__NT__) \
       && !defined(_WINDOWS)
       #if !defined(OS2)*/
-  typedef unsigned char  BYTE;
-  typedef unsigned long  DWORD;
+typedef unsigned char  BYTE;
+typedef unsigned long  DWORD;
 /* #endif*/
- typedef unsigned short WORD;
+typedef unsigned short WORD;
 /*#else
  #define WIN32_LEAN_AND_MEAN
  #define NOGDI
