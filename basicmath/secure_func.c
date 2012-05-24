452483269d9fb08ecb9c3cac41d29c79f05a58f0
Ethan Chen <intervigil@gmail.com>
2012-05-23 23:25:03 -0700

    basicmath: add common secure function files

---
+++ basicmath/secure_func.c
@@ -0,0 +1,77 @@
+/*
+ * implementation for secure OR32 functions
+ */
+
+#include "secure_func.h"
+
+int secure_add(int a, int b)
+{
+  asm volatile("l.addx %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+int secure_sub(int a, int b)
+{
+  asm volatile("l.subx %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+int secure_mul(int a, int b)
+{
+  asm volatile("l.mulx %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+int secure_div(int a, int b)
+{
+  asm volatile("l.divx %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+double secure_fp_add(double a, double b)
+{
+  asm volatile("lf.addx.s %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+double secure_fp_sub(double a, double b)
+{
+  asm volatile("lf.subx.s %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+double secure_fp_mul(double a, double b)
+{
+  asm volatile("lf.mulx.s %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
+
+double secure_fp_div(double a, double b)
+{
+  asm volatile("lf.divx.s %0,%1,%2;"
+               : "=r"(a)
+               : "r"(a), "r"(b)
+               );
+  return a;
+}
---
+++ basicmath/secure_func.h
@@ -0,0 +1,13 @@
+/*
+ * header file for using secure OR32 functions
+ */
+
+int secure_add(int a, int b);
+int secure_sub(int a, int b);
+int secure_mul(int a, int b);
+int secure_div(int a, int b);
+
+double secure_fp_add(double a, double b);
+double secure_fp_sub(double a, double b);
+double secure_fp_mul(double a, double b);
+double secure_fp_div(double a, double b);
