f864c2fd2fdf294b4bf956a5d32f6222ec41ea9c
Ethan Chen <intervigil@gmail.com>
2012-05-30 21:44:35 -0700

    crc32: count average differing bits

--- crc32/main.c
+++ crc32/main.c
@@ -16,10 +16,14 @@ int
 main(int argc, char *argv[]) {
     DWORD crc, secure_crc;
     long charcnt;
-    int i, j;
+    int i, j, k;
     register errors = 0;
     unsigned int ticks;
     char in_buf[CHUNK_SIZE];
+#ifdef USE_SECURE
+    int bit_cnt = 0;
+    int diff_cnt = 0;
+#endif
 
     srand(1337);
 
@@ -37,12 +41,23 @@ main(int argc, char *argv[]) {
         secure_crc = crc32(in_buf, CHUNK_SIZE, 1);
         crc = crc32(in_buf, CHUNK_SIZE, 0);
         printf("CRC32: 0x%.8x, expected 0x%.8x\n", secure_crc, crc);
+
+        bit_cnt = 0;
+        for (k = 0; k < sizeof(secure_crc) * 4; k++) {
+            if (((secure_crc >> k) & 0x1) != ((crc >> k) & 0x1))
+                bit_cnt++;
+        }
+        diff_cnt += bit_cnt;
+        printf("CRC32: differing bits: %d\n", bit_cnt);
 #else
         crc = crc32(in_buf, CHUNK_SIZE, 0);
         printf("CRC32: 0x%.8x\n", crc);
 #endif
     }
     printf("Finished %d CRC32 iterations\n", ITERATIONS);
+#ifdef USE_SECURE
+    printf("Average differing bits: %d\n", diff_cnt / ITERATIONS);
+#endif
 
     ticks = or1k_timer_get_ticks();
     printf("Elapsed: %d ticks at %d Hz\n", ticks, TIMER_HZ);
